3aa8347f98f4a29823a29e7e32a1d63f599e7c30
tiendk195 <tiendk195@gmail.com>
2024-05-01 14:45:08 +0700

    Add SignIn/Out with gooogle account by Next-Auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,30 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-const handle = NextAuth({
+const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId:
+        process.env.GOOGLE_CLIENT_ID ??
+        "1033135223027-gcld3mactbhri64futmuq7c2jrifpv1l.apps.googleusercontent.com",
+      clientSecret:
+        process.env.GOOGLE_CLIENT_SECRET ??
+        "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",
     }),
   ],
+  callbacks: {
+    jwt: ({ token, account }) => {
+      if (account?.access_token) {
+        token.access_token = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.access_token = token.access_token;
+      session.user.id = token.id;
+      return session;
+    },
+  },
 });
 
-export { handle as GET, handle as POST };
+export { handler as GET, handler as POST };
--- app/page.js
+++ app/page.js
@@ -1,8 +1,16 @@
-import { Main } from "next/document";
 import Image from "next/image";
+import Appbar from "./components/AppBar";
 
 export default function Home() {
   return (
-    <main className="ng-main-gray flex flex-col min-h-screen text-black"></main>
+    <main className="ng-main-gray flex flex-col min-h-screen text-black">
+      <Appbar />
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-main-grey min-h-screen">
+          <p className="m-4 text-[4rem] font-bold font-mono">Hello World</p>
+          <p className="m-4 text-[1.5rem] font-normal font-mono">description</p>
+        </div>
+      </div>
+    </main>
   );
 }
